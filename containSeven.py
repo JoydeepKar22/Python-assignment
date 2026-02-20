4cd62c2b8ebce30f91cc07e9a5dc34d5af0afb00
JoydeepKar22 <joydeepkar16@gmail.comm>
2026-02-21 00:05:58 +0530

    first commit: ContainSeven.py

---
+++ containSeven.py
@@ -0,0 +1,12 @@
+#print all numbers between 1 and 100 that contain the digit 7
+
+num = 1
+while(num<100):
+    s = str(num)
+    if '7' in s :
+        print(s)
+    num +=1    
+    
+    
+   
+    
